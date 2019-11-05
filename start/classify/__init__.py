41ba77b81d2f50273c22447690e1eb92bbaf3577
Sonia Kulkarni <kulkarnisonia16@gmail.com>
2019-11-05 14:22:41 -0500

    Initial commit

---
+++ start/classify/__init__.py
@@ -0,0 +1,15 @@
+import logging
+import azure.functions as func
+import json
+from .predict import predict_image_from_url
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    image_url = req.params.get('img')
+    results = predict_image_from_url(image_url)
+
+    headers = {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    }
+    return func.HttpResponse(json.dumps(results), headers = headers)
